701fd38018d1fd983fbf33e1b3e694e1fd64165f
Malcolm Ross <MJRoss@student.fullsail.edu>
2018-08-01 20:18:11 -0400

    Added input validation

--- dynamicFormBuilder.js
+++ dynamicFormBuilder.js
@@ -11,38 +11,45 @@ var formElementsArr = [
 
 // Iterates through an array of objects representing form elements and outputs the form elements to the console.
 function generateForm (formElements) {
-	var elementsString = "<form>";
-	var element;
+	if (Array.isArray(formElements)){
+		var elementsString = "<form>";
+		var element;
 
-	for(var i = 0; i < formElements.length; i++){
-		if(formElements[i].element == "input") {
-			if(formElements[i].type == "text"){
-				element = "<input type='text' />";
-			}else if(formElements[i].type == "email"){
-				element = "<input type='email' />";
-			}else if(formElements[i].type == "password"){
-				element = "<input type='password' />";
-			}else if(formElements[i].type == "submit"){
-				element = "<input type='submit' />";
+		for(var i = 0; i < formElements.length; i++){
+			if (typeof formElements[1] === 'object' && formElements[i].element){
+				if(formElements[i].element == "input") {
+					if(formElements[i].type == "text"){
+						element = "<input type='text' />";
+					}else if(formElements[i].type == "email"){
+						element = "<input type='email' />";
+					}else if(formElements[i].type == "password"){
+						element = "<input type='password' />";
+					}else if(formElements[i].type == "submit"){
+						element = "<input type='submit' />";
+					} else {
+						element = "<input type='' />"
+					}
+				} else if (formElements[i].element == "textarea"){
+					element = "<textarea></textarea>"
+				} else if (formElements[i].element == "button"){
+					element = "<button></button>"
+				} else if (formElements[i].element == "select"){
+					element = "<select>";
+					for(var j = 0; j < formElements[i].optVals; j++){
+						element += "<option value=''></option>";
+					}
+					element += "</select>";
+				}
+				elementsString += element;
 			} else {
-				element = "<input type='' />"
+				console.log("Element at position " + i + " must be an object");
 			}
-		} else if (formElements[i].element == "textarea"){
-			element = "<textarea></textarea>"
-		} else if (formElements[i].element == "button"){
-			element = "<button></button>"
-		} else if (formElements[i].element == "select"){
-			element = "<select>";
-			for(var j = 0; j < formElements[i].optVals; j++){
-				element += "<option value=''></option>";
-			}
-			element += "</select>";
 		}
-		//console.log(element);
-		elementsString += element;
+		elementsString += "</form>";
+		console.log(elementsString);
+	} else {
+		console.log("Must be an array.");
 	}
-	elementsString += "</form>";
-	console.log(elementsString);
 }
 
 generateForm(formElementsArr);
