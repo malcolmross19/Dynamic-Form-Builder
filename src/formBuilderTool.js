a94ca3c8696c15ea24a4ebd202dccf9689a9a2c8
Malcolm Ross <MJRoss@student.fullsail.edu>
2018-08-04 21:59:02 -0400

    Removed unnecessary comments

--- src/formBuilderTool.js
+++ src/formBuilderTool.js
@@ -1,3 +1,4 @@
+// Adds <option></option> elements inside <select></select> elements.
 function addOptions(elem, numOptions){
 	elem = "<select>";
 	for(var j = 0; j < numOptions; j++){
@@ -7,23 +8,21 @@ function addOptions(elem, numOptions){
 	return elem;
 }
 
+// Parses array of html form element placeholders and outputs string containing the html for a form
 exports.gen = (array) => {
-    //const formattedString = array[0];
-    if (Array.isArray(array)){
+	if (Array.isArray(array)){
 		var elementsString = "<form>";
 		var element;
 
 		for(var i = 0; i < array.length; i++) { // Refactored conditional if statements to ternary operators
-			element = (typeof array[i] === 'object' && array[i].element ? (array[i].element == "input" ? (array[i].type == "text" ? "<input type='text' />" : (array[i].type == "email") ? 
-			"<input type='email' />" : (array[i].type == "password") ? "<input type='password' />" : (array[i].type == "submit") ? "<input type='submit' />" : "<input type='' />") : (array[i].element == "textarea") ? 
-			"<textarea></textarea>" : (array[i].element == "button") ? "<button></button>" : (array[i].element == "select") ? addOptions(element, array[i].optVals) : "") : 
-			console.log("Element at position " + i + " must be an object"));
+			element = (typeof array[i] === "object" && array[i].element ? (array[i].element == "input" ? (array[i].type == "text" ? "<input type='text' />" : (array[i].type == "email") ? 
+				"<input type='email' />" : (array[i].type == "password") ? "<input type='password' />" : (array[i].type == "submit") ? "<input type='submit' />" : "<input type='' />") : (array[i].element == "textarea") ? 
+				"<textarea></textarea>" : (array[i].element == "button") ? "<button></button>" : (array[i].element == "select") ? addOptions(element, array[i].optVals) : "") : 
+				"");
 
 			elementsString += element;
 		}
 		elementsString += "</form>";
-	} else {
-		console.log("Must be an array.");
 	}
-    return elementsString;
-}
+	return elementsString;
+};
